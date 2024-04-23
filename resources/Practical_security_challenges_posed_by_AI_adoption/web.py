99f6c3fa2a23e7a0d9804f8b85cfbc25642b5513
inverzeio <48851963+inverzeio@users.noreply.github.com>
2024-04-24 01:29:49 +0200

    Create web.py
    Original, ChatGPT 4.0

---
+++ resources/Practical_security_challenges_posed_by_AI_adoption/web.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///uploads.db'
+app.config['UPLOAD_FOLDER'] = 'uploads'
+db = SQLAlchemy(app)
+
+class Document(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(100))
+
+db.create_all()
+
+@app.route('/')
+def index():
+    documents = Document.query.all()
+    return render_template('index.html', documents=documents)
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+
+    file = request.files['file']
+
+    if file.filename == '':
+        return redirect(request.url)
+
+    if file:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        document = Document(filename=filename)
+        db.session.add(document)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return render_template('uploaded_file.html', filename=filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
