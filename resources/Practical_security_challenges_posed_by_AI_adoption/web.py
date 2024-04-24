abd6627918bccbe936fec8da5bda5b791d07b2ae
inverzeio <48851963+inverzeio@users.noreply.github.com>
2024-04-24 02:28:47 +0200

    Update web.py
    Potentially better solutions

--- resources/Practical_security_challenges_posed_by_AI_adoption/web.py
+++ resources/Practical_security_challenges_posed_by_AI_adoption/web.py
@@ -11,7 +11,7 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS #https://stackoverflow.com/a/51384608/12534623
     
 class Document(db.Model):
     id = db.Column(db.Integer, primary_key=True)
